96127ff9821e34de364ca5b9ac2d8690ba5af9d0
Dmitry Yanchenkov <yanchenkov.dmitry@gmail.com>
2022-12-06 12:38:41 +0300

    refactor day6

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,25 +1,12 @@
 fun main() {
-    val size1 = 4
-    input("Day6")
-        .readText()
-        .windowed(size1)
-        .map(String::toSet)
-        .indexOfFirst {
-             it.size == size1
-        }
-        .also {
-            println(it + size1)
-        }
-
-    val size2 = 14
-    input("Day6")
-        .readText()
-        .windowed(size2)
-        .map(String::toSet)
-        .indexOfFirst {
-             it.size == size2
-        }
-        .also {
-            println(it + size2)
-        }
+    listOf(4, 14).forEach { size ->
+        input("Day6").readText()
+            .windowed(size, transform = CharSequence::toSet)
+            .indexOfFirst {
+                it.size == size
+            }
+            .also {
+                println(it + size)
+            }
+    }
 }
