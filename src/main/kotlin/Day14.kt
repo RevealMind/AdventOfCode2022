75be2aefc5c76bfa6997130964ad244109e91f17
Dmitry Yanchenkov <yanchenkov.dmitry@gmail.com>
2022-12-15 03:10:02 +0300

    add day14

---
+++ src/main/kotlin/Day14.kt
@@ -0,0 +1,103 @@
+import kotlin.math.*
+
+private data class Point(val x: Int, val y: Int) {
+    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+}
+
+private fun Point.to(other: Point) = (min(x, other.x)..max(x, other.x)).flatMap { x ->
+    (min(y, other.y)..max(y, other.y)).map { y ->
+        Point(x, y)
+    }
+}
+
+private val Iterable<Point>.border
+    get() = run {
+        val minX = this.minOf { it.x }
+        val minY = this.minOf { it.y }
+        val maxX = this.maxOf { it.x }
+        val maxY = this.maxOf { it.y }
+        Point(minX, minY) to Point(maxX, maxY)
+    }
+
+private class Map(val rocks: Set<Point>, val start: Point) {
+    val border = (rocks + start).border
+
+    private fun Point.isInBorder() =
+        x in border.first.x..border.second.x && y in border.first.y..border.second.y
+
+    fun simulate1() {
+        var curPosition = start
+        val blocks: MutableSet<Point> = rocks.toMutableSet()
+        var cnt = 0
+        while (true) {
+            val down = curPosition + DOWN
+            val left = curPosition + LEFT_DIAG
+            val right = curPosition + RIGHT_DIAG
+            curPosition = if (down !in blocks) {
+                if (!down.isInBorder()) break
+                down
+            } else if (left !in blocks) {
+                if (!left.isInBorder()) break
+                left
+            } else if (right !in blocks) {
+                if (!right.isInBorder()) break
+                right
+            } else {
+                blocks.add(curPosition)
+                cnt++
+                if (curPosition == start) break
+                start
+            }
+        }
+        println("Sands: $cnt")
+    }
+
+    private fun Point.isOnFloor() =
+        y == border.second.y + 2
+
+    fun simulate2() {
+        var curPosition = start
+        val blocks: MutableSet<Point> = rocks.toMutableSet()
+        var cnt = 0
+        while (true) {
+            val down = curPosition + DOWN
+            val left = curPosition + LEFT_DIAG
+            val right = curPosition + RIGHT_DIAG
+            curPosition = if (down !in blocks && !down.isOnFloor()) {
+                down
+            } else if (left !in blocks && !left.isOnFloor()) {
+                left
+            } else if (right !in blocks && !right.isOnFloor()) {
+                right
+            } else {
+                blocks.add(curPosition)
+                cnt++
+                if (curPosition == start) break
+                start
+            }
+        }
+        println("Sands: $cnt")
+    }
+
+    companion object {
+        val DOWN = Point(0, 1)
+        val LEFT_DIAG = Point(-1, 1)
+        val RIGHT_DIAG = Point(1, 1)
+    }
+}
+
+
+fun main() {
+    val rocks = mutableSetOf<Point>()
+    input("Day14").readLines().forEach { line ->
+        rocks += line.split(" -> ", ",").chunked(2) { (x, y) -> Point(x.toInt(), y.toInt()) }
+            .windowed(2) { (left, right) ->
+                left.to(right)
+            }.flatten()
+    }
+
+    Map(rocks, Point(500, 0)).also {
+        it.simulate1()
+        it.simulate2()
+    }
+}
